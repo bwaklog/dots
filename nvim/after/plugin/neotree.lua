5a1384019021c95d3023fc6849fad5f6cc3fb1f0
Aditya Hegde <aditya.mh@outlook.com>
2024-04-15 11:04:03 +0530

    added lazy vim files

--- nvim/after/plugin/neotree.lua
+++
@@ -1,8 +0,0 @@
-require("neo-tree").setup ({
-    -- map leader nt to <cmd>NeoTree<cmd>
-
-    vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<CR>", { desc = "Toggle NeoTree" })
-
-    -- change width of neo-tree
-
-})
--- nvim/lua/bwaklog/remap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('t', '<C-w>', "<C-\\><C-n><C-w>h",{silent = true})
